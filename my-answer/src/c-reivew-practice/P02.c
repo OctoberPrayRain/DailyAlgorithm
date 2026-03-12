11de0c89bcf0d479ffaf9b1aa27cb0dff9ba6fa4
OctoberPrayRain <octoberprayrain@gmail.com>
2026-03-12 17:57:01 +0800

    feat(c-review-practive):C语言复习练习P02

--- my-answer/src/c-reivew-practice/P02.c
+++ my-answer/src/c-reivew-practice/P02.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-int main(){
-    
 
+int main(){
+    int count = 0; //count the number's appearance time.
+    char str[1000];
+    if(fgets(str,sizeof(str),stdin) != NULL){
+        size_t len = strlen(str);
+        for(int i = 0 ; i < len ; i++){
+            if(str[i]<= '9' && str[i] >= '0')
+                count++;
+        }
+    }
+    printf("%d",count);
     return 0;
 }
